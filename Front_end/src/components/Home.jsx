ad3734b9bdc7d22e62311e5ed0f4b8afd1815a7f
VuLeHong <144672834+VuLeHong@users.noreply.github.com>
2024-03-30 19:23:02 +0700

    Vu le hong (#3)
    * generatae
    
    * next step

---
+++ Front_end/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from './Task/Task'
+import Rank from './Rank/Rank'
+function Home() {
+  return (
+    <div className='Home'>
+          <Task/>
+          <Rank/> 
+        
+    </div>
+  )
+}
+
+export default Home
