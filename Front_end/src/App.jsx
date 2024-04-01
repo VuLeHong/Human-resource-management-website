f9c9f90f37e0296da2162f03d7e308fc9f1ebcfb
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-04-01 08:18:05 +0700

    merge navbar

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home'
+import { useState, React, useEffect } from 'react';
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar/Navbar';
 function App() {
+  const current_theme = localStorage.getItem('current_theme')
+  const [theme, setTheme] = useState(current_theme?
+    current_theme : 'light');
+
+    useEffect(() => {
+      localStorage.setItem('current_theme', theme)
+    }, [theme])
 
   return (
     <>
       <div>
-        <Home/>
+        <div className={`container ${theme}`}>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <Home/>
+        </div>
+        {/* <Home/> */}
       </div>
     </>
   )
 }
 
-export default App
+export default App;
