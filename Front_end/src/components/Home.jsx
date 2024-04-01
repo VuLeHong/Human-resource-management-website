706596a636181b44361efa946470d1ffa80ee8e1
hdhjshsh <151993635+hdhjshsh@users.noreply.github.com>
2024-04-01 08:06:42 +0700

    mini-profile (#4)

--- Front_end/src/components/Home.jsx
+++ Front_end/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Task from './Task/Task'
 import Rank from './Rank/Rank'
+import Mini_profile from './Mini_profile/Mini_profile'
 function Home() {
   return (
     <div className='Home'>
+          <Mini_profile/>
           <Task/>
-          <Rank/> 
+          <Rank/>
         
     </div>
   )
