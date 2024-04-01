8d4ccfbd323af82f463ec97293f4637aa0952b66
nhad1702 <90076670+nhad1702@users.noreply.github.com>
2024-04-01 08:52:21 +0700

    merge home page (#8)
    * merge navbar
    
    * merge Navbar fixed
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: VuLeHong <144672834+VuLeHong@users.noreply.github.com>

--- Front_end/src/components/Home.jsx
+++ Front_end/src/components/Home.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Task from './Task/Task'
-import Rank from './Rank/Rank'
+
+import { React, useState, useEffect } from 'react';
+import Task from './Task/Task';
+import Rank from './Rank/Rank';
+import Navbar from '../components/Navbar/Navbar';
 import Mini_profile from './Mini_profile/Mini_profile'
 function Home() {
+
+  const current_theme = localStorage.getItem('current_theme');
+  const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
+
+  useEffect(() => {
+    localStorage.setItem('current_theme', theme)
+  }, [theme]);
+
   return (
-    <div className='Home'>
-          <Mini_profile/>
-          <Task/>
-          <Rank/>
-        
+    <div>
+      <div className={`container ${theme}`}>
+          <Navbar theme={theme} setTheme={setTheme} />
+      </div>
+      <Mini_profile/>
+      <div className='Home'>
+            <Task/>
+            <Rank/>   
+      </div>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
