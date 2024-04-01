d6ebeb17f0ae33f84820c55eeb4bf0d91b145550
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-04-01 08:31:59 +0700

    merge Navbar fixed

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,24 +1,11 @@
-import { useState, React, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
-import Navbar from './components/Navbar/Navbar';
 function App() {
-  const current_theme = localStorage.getItem('current_theme')
-  const [theme, setTheme] = useState(current_theme?
-    current_theme : 'light');
-
-    useEffect(() => {
-      localStorage.setItem('current_theme', theme)
-    }, [theme])
-
   return (
     <>
       <div>
-        <div className={`container ${theme}`}>
-          <Navbar theme={theme} setTheme={setTheme} />
-          <Home/>
-        </div>
-        {/* <Home/> */}
+        <Home/>
       </div>
     </>
   )
--- Front_end/src/components/Home.jsx
+++ Front_end/src/components/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import Task from './Task/Task'
-import Rank from './Rank/Rank'
+import {React, useState, useEffect} from 'react';
+import Task from './Task/Task';
+import Rank from './Rank/Rank';
+import Navbar from './components/Navbar/Navbar';
 function Home() {
+
+  const current_theme = localStorage.getItem('current_theme')
+  const [theme, setTheme] = useState(current_theme?
+    current_theme : 'light');
+
+    useEffect(() => {
+      localStorage.setItem('current_theme', theme)
+    }, [theme])
+
   return (
-    <div className='Home'>
-          <Task/>
-          <Rank/> 
-        
+    <div>
+      <div className={`container ${theme}`}>
+          <Navbar theme={theme} setTheme={setTheme} />
+      </div>
+      <div className='Home'>
+            <Task/>
+            <Rank/>   
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
