dad57ed6fbb3ba91e3cf93be68a5f80a19af14e8
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-04-17 13:51:29 +0700

    new UI

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
-import './App.css';
-import Home from './components/Home';
-import Login from './components/Login/Login';
+import { useState } from 'react'
+import './App.css'
+import Header from './components/head/Header'
+import Sidebar from './components/sidebar/Sidebar'
+// import Home from './Home'
+import Profile from './components/profile/Profile'
+import Task from './components/task/Task'
+import Ranking from './components/ranking/Ranking'
+import Project from './components/pr0ject/Project'
+
+
 function App() {
+
   return (
-      <div>
-        <Home/>
-        {/* <Login/> */}
-      </div>
+    <div className='grid-container'>
+      <Header />
+      <Sidebar />
+      {/* <Home /> */}
+      <Profile /> 
+      <Task />
+      {/* <Ranking />
+      <Project /> */}
+    </div>
   )
 }
 
-export default App;
+export default App
