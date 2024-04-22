bdbacb6a8ec64064892e163295d538f9cd6052cc
BaoWhiteHat <23560003@gm.uit.edu.vn>
2024-04-22 11:12:03 +0700

    task

--- Front_end/src/components/Task/Task.jsx
+++ Front_end/src/components/Task/Task.jsx
@@ -1,15 +1,55 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import "./Task.css"
 
-const Task = () => {
+const ToDoList = () => {
 
-  const [Task, setTask] = useState("");
+  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+
+  }
+
+  function CommentTask(index) {
+
+  }
+
+  function moveTaskUp(index) {
+
+  }
+
+
 
   return (
-    <>
-      <div className="task">
-      </div>
-    </>
+    <div className='to-do-list'>
+
+      <h1>UIT Evaluation System</h1>
+
+      <ol>
+        {tasks.map((task, index) =>
+        <li key={index}>
+            <span className="text">{task}</span>
+            <button
+            className='comment-button'
+            onClick={() => CommentTask(index)}>
+              Comment
+            </button>
+
+            <button
+            className='move-button'
+            onClick={() => CommentTask(index)}>
+              Upload
+            </button>
+        </li>
+      )}
+      </ol>
+
+    </div>
   )
 }
 
-export default Task
+export default ToDoList
