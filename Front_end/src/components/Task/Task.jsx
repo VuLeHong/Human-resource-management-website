7721d558422e4d4475944e526af4325101ae7375
nhad1702 <90076670+nhad1702@users.noreply.github.com>
2024-04-22 20:20:08 +0700

    Duc (#19)
    * mini profile
    
    * merge mini profile
    
    * fix profile
    
    * .
    
    * temp fix display
    
    * again
    
    * fix UI
    
    * fix UI
    
    * command
    
    * ahihi
    
    * new UI
    
    * note
    
    * .
    
    * >
    
    * delete popup
    
    * ranking
    
    * appear total stats score
    
    * ranking logic attempt
    
    * logic sort finished
    
    ---------
    
    Co-authored-by: nhad1702 <anhducnhsg@gmail.com>
    Co-authored-by: kiiiiiiiiif <kyle.vuanh@gmail.com>
    Co-authored-by: VuLeHong <23560046@gm.uit.edu.vn>
    Co-authored-by: VuLeHong <144672834+VuLeHong@users.noreply.github.com>

--- Front_end/src/components/Task/Task.jsx
+++ Front_end/src/components/Task/Task.jsx
@@ -1,26 +1,56 @@
-import React, { useState } from 'react'
-import './Task.css'
 
-function Task() {
-    const [tasks, setTasks] = useState([])
-    const auth = localStorage.getItem("user");
-    const auth1 = JSON.parse(auth);
-    const t = auth1.tasks;
-    
+import React, {useState} from 'react'
+import './Task.css'
+import user from '../profile/mockData';
+
+const Task = () => {
+
+  const taskNum = user[0].tasks.length
+
+  // for (let i = 0; i < taskNum; i++) {
+  //   return(
+  //     <h3>{user[0].tasks[i]}</h3>
+  //   )
+  // }
+
+  const numTask = () => {
+    for (let i = 1; i <= user[0].tasks.length; i++) {
+        return (<h3>{user[0].tasks[i]}</h3>)
+    }
+  }
 
   return (
-    <div className='tasking'>
-        {
-            t.length === 0 ?
-            <div><h2>No Task</h2></div>
-            :
-            t.map(task =>(
-                <div>
-                    <p>{task.content}</p>
-                </div>
-            ))
-        }
-    </div>
+    <>
+      <div className="task">
+      <h1>Hi {user[0].fullName}, here's your task list</h1>
+      <h3>Today you have {user[0].tasks.length} tasks</h3>
+        {user[0].tasks.length > 0 ? 
+          <div className="list">
+            
+          </div> 
+        : <h3 className='alert'>No task to do</h3>}
+      </div>
+    </>
+// function Task() {
+//     const [tasks, setTasks] = useState([])
+//     const auth = localStorage.getItem("user");
+//     const auth1 = JSON.parse(auth);
+//     const t = auth1.tasks;
+    
+
+//   return (
+//     <div className='tasking'>
+//         {
+//             t.length === 0 ?
+//             <div><h2>No Task</h2></div>
+//             :
+//             t.map(task =>(
+//                 <div>
+//                     <p>{task.content}</p>
+//                 </div>
+//             ))
+//         }
+//     </div>
   )
 }
 
--- Front_end/src/components/ranking/Ranking.jsx
+++ Front_end/src/components/ranking/Ranking.jsx
@@ -1,3 +1,5 @@
+
+import user from '../profile/mockData'
 import './Ranking.css'
 import axios from "axios";
 import { React, useState, useEffect } from 'react';
@@ -24,24 +26,79 @@ function Ranking() {
     getUsers();
   }, []);
 
+const Ranking = () => {
+
+  const avatar = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-541.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais";
+  
+  const statsMap = user.map(employee => ({
+    "organizational_skill": employee.stats.organizational_skill, 
+    "techical_skill": employee.stats.techical_skill,
+    "idea_contribution": employee.stats.idea_contribution,
+    "communication_skill": employee.stats.communication_skill,
+    "product_optimization": employee.stats.product_optimization
+  }))
+
+  const sumArr = []
+  statsMap.forEach(obj => {
+    let objSum = 0
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        objSum += obj[key]
+      }
+    }
+    sumArr.push(objSum)
+  })
+
+  // for (let i = 0; i < sumArr.length; i++) {
+  //   user[i].score = (user[i].score || 0) + sumArr[i]
+  // }
+  // user.sort((a, b) => b.score - a.score)
+
   return (
-    <div className='ranking-container'>
-        <h2>Ranking</h2>
-          <div className='ranking'>
-          {
-            users.length === 0 ?
-            <div><h2>No Ranking</h2></div>
-            :
-            users.map(user =>(
-                <div>
-                    <p>{user.truename}</p>
-                    <p>{user.role}</p>
-                    <p>{user.rank}</p>
-                </div>
-            ))
-          }
+    <div className='ranking'>
+        <div className="title">
+          <h1>Hello {user[0].fullName}, here is your total stats!</h1>
+        </div>
+        <div className="rank">
+          <div className="avatar">
+            <img src={avatar} alt="" />
           </div>
-      </div>
+          <div className="rank">
+            {/* <h3>{}</h3> */}
+          </div>
+          <div className="name">
+            <h3>{user[0].fullName}</h3>
+          </div>
+          <div className="total">
+            <h5>{sumArr[0]}</h5>
+          </div>
+        </div>
+        <div className='ladderboard'>
+          <div className="subTitle">
+            <h1>Ladderboard</h1>
+          </div>
+          <div className="dashBoard"></div>
+        </div>
+    </div>
+
+//   return (
+//     <div className='ranking-container'>
+//         <h2>Ranking</h2>
+//           <div className='ranking'>
+//           {
+//             users.length === 0 ?
+//             <div><h2>No Ranking</h2></div>
+//             :
+//             users.map(user =>(
+//                 <div>
+//                     <p>{user.truename}</p>
+//                     <p>{user.role}</p>
+//                     <p>{user.rank}</p>
+//                 </div>
+//             ))
+//           }
+//           </div>
+//       </div>
   )
 }
 
