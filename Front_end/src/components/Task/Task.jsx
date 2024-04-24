d5c49ae406d166e79bf409ac0fa032977ee6e8d7
Le Quoc Bao <145678921+BaoWhiteHat@users.noreply.github.com>
2024-04-24 13:39:15 +0700

    Task (#18)
    * ok
    
    * ok
    
    * Login nha
    
    * ok
    
    * ok
    
    * ok
    
    * task
    
    * task update
    
    * task
    
    ---------
    
    Co-authored-by: kiiiiiiiiif <kyle.vuanh@gmail.com>
    Co-authored-by: VuLeHong <144672834+VuLeHong@users.noreply.github.com>

--- Front_end/src/components/Task/Task.jsx
+++ Front_end/src/components/Task/Task.jsx
@@ -1,36 +1,56 @@
 
-import React, {useState} from 'react'
-import './Task.css'
-import user from '../profile/mockData';
+import React, { useState } from 'react'
+import "./Task.css"
 
-const Task = () => {
+const ToDoList = () => {
 
-  const taskNum = user[0].tasks.length
+  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+  const [newTask, setNewTask] = useState("");
 
-  // for (let i = 0; i < taskNum; i++) {
-  //   return(
-  //     <h3>{user[0].tasks[i]}</h3>
-  //   )
-  // }
-
-  const numTask = () => {
-    for (let i = 1; i <= user[0].tasks.length; i++) {
-        return (<h3>{user[0].tasks[i]}</h3>)
-    }
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
   }
 
+  function addTask() {
+
+  }
+
+  function CommentTask(index) {
+
+  }
+
+  function moveTaskUp(index) {
+
+  }
+
+
+
   return (
-    <>
-      <div className="task">
-      <h1>Hi {user[0].fullName}, here's your task list</h1>
-      <h3>Today you have {user[0].tasks.length} tasks</h3>
-        {user[0].tasks.length > 0 ? 
-          <div className="list">
-            
-          </div> 
-        : <h3 className='alert'>No task to do</h3>}
-      </div>
-    </>
+    <div className='to-do-list'>
+
+      <h1>UIT Evaluation System</h1>
+
+      <ol>
+        {tasks.map((task, index) =>
+        <li key={index} className="seperate_button">
+            <span className="text">{task}</span>
+            <button
+            className='comment-button'
+            onClick={() => CommentTask(index)}>
+              Comment
+            </button>
+
+            <button
+            className='move-button'
+            onClick={() => CommentTask(index)}>
+              Upload
+            </button>
+        </li>
+      )}
+      </ol>
+
+    </div>
+
 // function Task() {
 //     const [tasks, setTasks] = useState([])
 //     const auth = localStorage.getItem("user");
@@ -54,4 +74,6 @@ const Task = () => {
   )
 }
 
-export default Task
+// Có mồi function backend rồi á
+
+export default ToDoList
