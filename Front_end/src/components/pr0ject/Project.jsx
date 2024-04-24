a407ca35f8a8ecd40754ebdebcffcc1dca86a8b7
Le Quoc Bao <145678921+BaoWhiteHat@users.noreply.github.com>
2024-04-24 15:53:55 +0700

    Le quoc bao (#25)
    * ok
    
    * ok
    
    * Login nha
    
    * ok
    
    * ok
    
    * ok
    
    * task
    
    * task update
    
    * task
    
    * task
    
    * ok
    
    ---------
    
    Co-authored-by: kiiiiiiiiif <kyle.vuanh@gmail.com>
    Co-authored-by: VuLeHong <144672834+VuLeHong@users.noreply.github.com>

--- Front_end/src/components/pr0ject/Project.jsx
+++ Front_end/src/components/pr0ject/Project.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Project.css"
+import { } from 'react-icons/bs'
+import { IoSearchSharp } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+
+
 
 const Project = () => {
+
+  const [tasks, setTasks] = useState(["Making Front_end", "Merge", "DCM"]);
+  const [newTask, setNewTask] = useState("");
+  const commentEl = document.getElementById("commentEl");
+
+  const commentTask = () => {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>Hello VSCode</h1>
+    <div className='project'>
+
+        <button className='create' onClick="">
+          <FaPlus />
+          Create Project
+        </button>
+
+      <div>
+        {tasks.map((task, index) =>
+          <div key={index} className="view-project-btn">
+            <div className="project-list">
+              <div className='task-name'>
+                <h1 className="text">{task}</h1>
+              </div>
+              <div className="btn">
+                <button className='move-btn' onClick="">
+                  View Details <IoSearchSharp />
+                </button>
+              </div>
+            </div>
+            <div className="comment" id='comment'>
+
+            </div>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
