47d8b6bcc06466899de4bfc812158c1af63d3904
BaoWhiteHat <23560003@gm.uit.edu.vn>
2024-04-24 13:50:55 +0700

    task

--- Front_end/src/components/Task/Task.jsx
+++ Front_end/src/components/Task/Task.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react'
 import "./Task.css"
 
@@ -50,27 +49,6 @@ const ToDoList = () => {
       </ol>
 
     </div>
-
-// function Task() {
-//     const [tasks, setTasks] = useState([])
-//     const auth = localStorage.getItem("user");
-//     const auth1 = JSON.parse(auth);
-//     const t = auth1.tasks;
-    
-
-//   return (
-//     <div className='tasking'>
-//         {
-//             t.length === 0 ?
-//             <div><h2>No Task</h2></div>
-//             :
-//             t.map(task =>(
-//                 <div>
-//                     <p>{task.content}</p>
-//                 </div>
-//             ))
-//         }
-//     </div>
   )
 }
 
