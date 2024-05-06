b8d3f249fe83c5dd88fdcabd1be393408902a3c3
VuLeHong <23560046@gm.uit.edu.vn>
2024-05-06 10:43:49 +0700

    >

---
+++ Back_end/model/task.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const Taskschema = new mongoose.Schema({
+    content:{
+        type: String,
+        required: true,
+        default: ''
+    }, 
+    rank:{
+        type: String,
+        required:true,
+        default: 'E'
+    },
+    isdone:{
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    t_desc:{
+        type:String,
+        default:''
+    },
+    Project_id: {
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    }
+        
+})
+
+const Task_collection = mongoose.model("Task_collection",Taskschema)
+
+module.exports = Task_collection
