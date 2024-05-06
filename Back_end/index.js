e76d3bb3aa25bb50751d0c9b54caccf2e962536c
VuLeHong <144672834+VuLeHong@users.noreply.github.com>
2024-05-06 18:19:18 +0700

    Vu le hong (#35)
    * generatae
    
    * next step
    
    * profile
    
    * fix navbar
    
    * feat: Vuloz
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * backend of ranking
    
    * .
    
    * >
    
    * .
    
    * >
    
    * project and task api

--- Back_end/index.js
+++ Back_end/index.js
@@ -14,9 +14,9 @@ app.use(cors())
 app.use(express.json())
 
 app.post("/login", async(req, res) => {
-    const{username,password}=req.body
+    const{user_id,password}=req.body
     try{
-        const check = await user_collection.findOne({username:username})
+        const check = await user_collection.findOne({user_id:user_id})
         .then (check => {
             if(check){
                 if (check.password === password) {
@@ -66,35 +66,6 @@ app.post('/home', async(req, res) => {
     }
 })
 
-app.put('/home/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const user = await user_collection.findByIdAndUpdate(id, req.body);
-        // we cannot find any product in database
-        if(!user){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        const updatedUser = await user_collection.findById(id);
-        res.status(200).json(updatedUser);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.delete('/home/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const user = await user_collection.findByIdAndDelete(id);
-        if(!user){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        res.status(200).json(user);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
 
 app.post('/get', (req,res) =>{
     const {username} = req.body;
@@ -105,32 +76,51 @@ app.post('/get', (req,res) =>{
     .catch(err => res.json(err))
 })
 
-app.post('/updatedone', asyncHandler(async(req,res) =>{
-    const{username, index} = req.body;
-    const getUsertoChange = await user_collection.findOne({username: username});
-    update = getUsertoChange.tasks;
-    update[index].isdone = true;
-    console.log(update);
-    const works = await user_collection.findOneAndUpdate({username: username}, {tasks: update})
+//project
+app.post('/project', async(req, res) => {
+    try {
+        const result = await Project_collection.create(req.body)
+        res.status(200).json(result);
+    }
+    catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
+app.post('/pushtaskid', asyncHandler(async(req,res) =>{
+    const{ Project_id:Project_id, task_id: task_id} = req.body;
+    const task = {task_id};
+   const works = await Project_collection.findOneAndUpdate({Project_id: Project_id}, {$push: {tasks:task}})
     .then(result => res.json(result))
     .catch(err => res.json(err))
 }))
 
-app.post('/update', asyncHandler(async(req,res) =>{
-    const{username, index} = req.body;
-    const getUsertoChange = await user_collection.findOne({username: username});
-    update = getUsertoChange.tasks;
-    update[index].isdone = false;
-    console.log(update);
-    const works = await user_collection.findOneAndUpdate({username: username}, {tasks: update})
+//task 
+app.post('/addtask', asyncHandler(async(req,res) =>{
+    const{ content, rank, Project_id: Project_id, user_id:user_id} = req.body;
+    const task = {content: content, rank: rank, isdone: false ,Project_id: Project_id, user_id: user_id};
+   const works = await Task_collection.create(task)
     .then(result => res.json(result))
     .catch(err => res.json(err))
 }))
 
-app.post('/delete', asyncHandler(async(req,res) =>{
-    const{username, id} = req.body;
-    const task = {_id: id}
-    const works = await user_collection.findOneAndUpdate({username: username}, {$pull: {tasks:task}})
+app.get('/gettasks', async(req, res) => {
+    try {
+        const user = await Task_collection.find({});
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+app.post('/addcmt', asyncHandler(async(req,res) =>{
+    const{_id: _id, t_desc : t_desc} = req.body;
+   const works = await Task_collection.findOneAndUpdate({_id: _id}, {t_desc:t_desc})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+}))
+app.post('/updone', asyncHandler(async(req,res) =>{
+    const{_id: _id} = req.body;
+   const works = await Task_collection.findOneAndUpdate({_id: _id}, {isdone:true})
     .then(result => res.json(result))
     .catch(err => res.json(err))
 }))
