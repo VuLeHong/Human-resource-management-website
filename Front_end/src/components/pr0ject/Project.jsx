a67e14f9f27a252f98cce72c67f5ecc8e45a8fb4
unknown <binhlg.ti.2023@gmail.com>
2024-05-07 18:21:10 +0700

    new apis

--- Front_end/src/components/pr0ject/Project.jsx
+++ Front_end/src/components/pr0ject/Project.jsx
@@ -1,17 +1,29 @@
-
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Project.css"
 import { } from 'react-icons/bs'
 import { IoSearchSharp } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa6";
 import Sidebar from '../sidebar/Sidebar';
 import Header from '../head/Header';
+import axios from 'axios' 
 
 const Project = () => {
 
-  const [tasks, setTasks] = useState(["Making Front_end", "Merge", "DCM"]);
-  const [newTask, setNewTask] = useState("");
+  const [projects, setProjects] = useState([]);
   const commentEl = document.getElementById("commentEl");
+
+  const auth = localStorage.getItem("user");
+  const auth1 = JSON.parse(auth);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/getprojects') 
+    .then(result => {
+      console.log(result.data)
+      setProjects(result.data)
+      
+    })
+    .catch(err => console.log(err))
+  }, [])
 
   const commentTask = () => {
     return null;
@@ -22,34 +34,28 @@ const Project = () => {
       <Header />
       <Sidebar />
       <div className='project'>
-    <button className='create' onClick="">
-      <FaPlus />
-      Create Project
-    </button>
-
-<div>
-{tasks.map((task, index) =>
-  <div key={index} className="view-project-btn">
-    <div className="project-list">
-      <div className='task-name'>
-        <h1 className="text">{task}</h1>
+        <button className='create' onClick=""><FaPlus />Create Project</button>
+        <div>
+          {projects.map((project, index) =>
+            <div key={index} className="view-project-btn">
+              {project.owner_id === auth1.user_id ? 
+                <div className="project-list">
+                  <div className='task-name'>
+                    <h1 className="text">{project.name}</h1>
+                  </div>
+                  <div className="btn">
+                    <button className='move-btn' onClick="">View Details <IoSearchSharp /></button>
+                  </div>
+                </div>
+                :
+                <></>}
+              {/* <div className="comment" id='comment'>
+              </div> */}
+            </div>
+          )}
+        </div>
       </div>
-      <div className="btn">
-        <button className='move-btn' onClick="">
-          View Details <IoSearchSharp />
-        </button>
-      </div>
     </div>
-    <div className="comment" id='comment'>
-
-    </div>
-  </div>
-)}
-</div>
-
-</div>
-    </div>
-
   )
 }
 
--- Front_end/src/components/profile/Profile.jsx
+++ Front_end/src/components/profile/Profile.jsx
@@ -1,211 +1,26 @@
-// import React from 'react'
-// import './Profile.css'
-// import user from './mockData';
-// import Sidebar from '../sidebar/Sidebar';
-// import Header from '../head/Header';
-// const Profile = () => {
-
-//   const avatar = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-541.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais";
-//   const auth = localStorage.getItem("user");
-//   const auth1 = JSON.parse(auth);
-
-//   return (
-//     <div className='grid-container'>
-//       <Header />
-//       <Sidebar />
-//       <div className='contain'>
-//       <div className='profile-container'>
-//         <div className="profile">
-//           <div className='avatar'>
-//             <img src={avatar} height="250" width="250" alt="" />
-//           </div>
-//           <div className='details'>
-//             <div className="left">
-//               <div className="name">
-//                 <p>Name: {auth1.truename}</p>
-//                 {/* <p>Name: {user[user.length - 1].truename}</p> */}
-//               </div>
-//               <div className="gender">
-//                 {auth1.ismale === true ? <p>Gender: Male</p> : <p>Gender: Female</p>}
-//                 {/* {user[user.length - 1].ismale === true ? <p>Gender: Male</p> : <p>Gender: Female</p>} */}
-//               </div>
-//               <div className="staff">
-//                 <p>Role/Dept: {auth1.role}</p>
-//                 {/* <p>Role/Dept: {user[user.length - 1].role}</p> */}
-//               </div>
-//               <div className='process'>
-//                 <p>Rank: {auth1.rank}</p>
-//                 {/* <p>Rank: {user[user.length - 1].rank}</p> */}
-//                 <div className="progress-bar">
-//                   <progress value="50" max="100"></progress>
-//                   <p>50/100</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className='stats'>
-//             <ul>
-//               <p>Stats</p>
-//               <li>Communication skill: {auth1.stats.communication_skill}</li>
-//               <li>Idea contribution: {auth1.stats.idea_contribution}</li>
-//               <li>Organizational skill: {auth1.stats.organizational_skill}</li>
-//               <li>Product Optimization: {auth1.stats.product_optimization}</li>
-//               <li>Technology skill: {auth1.stats.techical_skill}</li>
-//               {/* <li>Communication skill: {user[user.length - 1].stats.communication_skill}</li>
-//               <li>Idea contribution: {user[user.length - 1].stats.idea_contribution}</li>
-//               <li>Organizational skill: {user[user.length - 1].stats.organizational_skill}</li>
-//               <li>Product Optimization: {user[user.length - 1].stats.product_optimization}</li>
-//               <li>Technology skill: {user[user.length - 1].stats.techical_skill}</li> */}
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-    
-//   )
-// }
-
-// export default Profile
-
-// import React from 'react'
-// import './Profile.css'
-// import Header from '../head/Header'
-// import Sidebar from '../sidebar/Sidebar'
-// const Profile = () => {
-
-//   const avatar = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-541.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais";
-//   const auth = localStorage.getItem("user");
-//   const auth1 = JSON.parse(auth);
-
-//   return (
-//     <div className='grid-container'>
-//       <Header />
-//       <Sidebar />
-//         <div className='contain'>
-//       <div className='profile-container'>
-//         <div className="profile">
-//           <div className='avatar'>
-//             <img src={avatar} height="250" width="250" alt="" />
-//           </div>
-//           <div className='details'>
-//             <div className="left">
-//               <div className="name">
-//                 <p>Name : {auth1.truename}</p>
-//               </div>
-//               <div className="gender">
-//                 {auth1.ismale === true ? <p>Gender : Male</p> : <p>Gender: Female</p>}
-//               </div>
-//               <div className="staff">
-//                 <p>Role/Dept : {auth1.role}</p>
-//               </div>
-//               <div className='process'>
-//                 <p>Rank : {auth1.rank}</p>
-//                 <div className="progress-bar">
-//                   <progress value="50" max="100"></progress>
-//                   <p>50/100</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className='stats'>
-//             <ul>
-//               <p className='job'>STATS : </p>
-//               <li>Communication skill: {auth1.stats.communication_skill}</li>
-//               <li>Idea contribution: {auth1.stats.idea_contribution}</li>
-//               <li>Organizational skill: {auth1.stats.organizational_skill}</li>
-//               <li>Product Optimization: {auth1.stats.product_optimization}</li>
-//               <li>Technology skill: {auth1.stats.techical_skill}</li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-    
-//   )
-// }
-
-// export default Profile
-// import React from 'react'
-// import './Profile.css'
-// import user from './mockData';
-// import Sidebar from '../sidebar/Sidebar';
-// import Header from '../head/Header';
-// const Profile = () => {
-
-//   const avatar = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-541.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais";
-//   const auth = localStorage.getItem("user");
-//   const auth1 = JSON.parse(auth);
-
-//   return (
-//     <div className='grid-container'>
-//       <Header />
-//       <Sidebar />
-//       <div className='contain'>
-//       <div className='profile-container'>
-//         <div className="profile">
-//           <div className='avatar'>
-//             <img src={avatar} height="250" width="250" alt="" />
-//           </div>
-//           <div className='details'>
-//             <div className="left">
-//               <div className="name">
-//                 <p>Name: {auth1.truename}</p>
-//                 {/* <p>Name: {user[user.length - 1].truename}</p> */}
-//               </div>
-//               <div className="gender">
-//                 {auth1.ismale === true ? <p>Gender: Male</p> : <p>Gender: Female</p>}
-//                 {/* {user[user.length - 1].ismale === true ? <p>Gender: Male</p> : <p>Gender: Female</p>} */}
-//               </div>
-//               <div className="staff">
-//                 <p>Role/Dept: {auth1.role}</p>
-//                 {/* <p>Role/Dept: {user[user.length - 1].role}</p> */}
-//               </div>
-//               <div className='process'>
-//                 <p>Rank: {auth1.rank}</p>
-//                 {/* <p>Rank: {user[user.length - 1].rank}</p> */}
-//                 <div className="progress-bar">
-//                   <progress value="50" max="100"></progress>
-//                   <p>50/100</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className='stats'>
-//             <ul>
-//               <p>Stats</p>
-//               <li>Communication skill: {auth1.stats.communication_skill}</li>
-//               <li>Idea contribution: {auth1.stats.idea_contribution}</li>
-//               <li>Organizational skill: {auth1.stats.organizational_skill}</li>
-//               <li>Product Optimization: {auth1.stats.product_optimization}</li>
-//               <li>Technology skill: {auth1.stats.techical_skill}</li>
-//               {/* <li>Communication skill: {user[user.length - 1].stats.communication_skill}</li>
-//               <li>Idea contribution: {user[user.length - 1].stats.idea_contribution}</li>
-//               <li>Organizational skill: {user[user.length - 1].stats.organizational_skill}</li>
-//               <li>Product Optimization: {user[user.length - 1].stats.product_optimization}</li>
-//               <li>Technology skill: {user[user.length - 1].stats.techical_skill}</li> */}
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-    
-//   )
-// }
-
-// export default Profile
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Profile.css'
 import Header from '../head/Header'
 import Sidebar from '../sidebar/Sidebar'
-const Profile = () => {
+import axios from 'axios' 
 
+function Profile (){
+  const [owner, setOwner] = useState({});
   const avatar = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-541.jpg?size=338&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais";
   const auth = localStorage.getItem("user");
   const auth1 = JSON.parse(auth);
+
+  
+  
+   useEffect(() => {
+    axios.post('http://localhost:5000/get', {user_id: auth1.user_id}) 
+    .then(result => {
+            setOwner(result.data)
+            console.log(owner.stats)
+    })
+    .catch(err => console.log(err))
+  },[])
+
 
   return (
     <div className='grid-container'>
@@ -220,16 +35,16 @@ const Profile = () => {
           <div className='details'>
             <div className="left">
               <div className="name">
-                <p>Name : {auth1.truename}</p>
+                <p>Name : {owner.truename}</p>
               </div>
               <div className="gender">
-                {auth1.ismale === true ? <p>Gender : Male</p> : <p>Gender: Female</p>}
+                {owner.ismale === true ? <p>Gender : Male</p> : <p>Gender: Female</p>}
               </div>
               <div className="staff">
-                <p>Role/Dept : {auth1.role}</p>
+                <p>Role/Dept : {owner.role}</p>
               </div>
               <div className='process'>
-                <p>Rank : {auth1.rank}</p>
+                <p>Rank : {owner.rank}</p>
                 <div className="progress-bar">
                   <progress value="50" max="100"></progress>
                   <p>50/100</p>
@@ -240,11 +55,11 @@ const Profile = () => {
           <div className='stats'>
             <ul>
               <p className='job'>STATS : </p>
-              <li>Communication skill: {auth1.stats.communication_skill}</li>
-              <li>Idea contribution: {auth1.stats.idea_contribution}</li>
-              <li>Organizational skill: {auth1.stats.organizational_skill}</li>
-              <li>Product Optimization: {auth1.stats.product_optimization}</li>
-              <li>Technology skill: {auth1.stats.techical_skill}</li>
+              <li>Organizational skill: {owner.stats === undefined ? auth1.stats.organizational_skill : owner.stats.organizational_skill}</li>
+              <li>Technology skill: {owner.stats === undefined ? auth1.stats.techical_skill : owner.stats.techical_skill}</li>
+              <li>Idea contribution: {owner.stats === undefined ? auth1.stats.idea_contribution : owner.stats.idea_contribution}</li>
+              <li>Communication skill: {owner.stats === undefined ? auth1.stats.communication_skill : owner.stats.communication_skill}</li>
+              <li>Product Optimization: {owner.stats === undefined ? auth1.stats.product_optimization : owner.stats.product_optimization}</li>
             </ul>
           </div>
         </div>
