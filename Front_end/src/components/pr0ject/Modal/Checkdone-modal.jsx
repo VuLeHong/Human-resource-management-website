8e2a86a47295aca623f6a7af12767e049cb5e31a
VuLeHong <23560046@gm.uit.edu.vn>
2024-05-14 09:32:24 +0700

    .

--- Front_end/src/components/pr0ject/Modal/Checkdone-modal.jsx
+++ Front_end/src/components/pr0ject/Modal/Checkdone-modal.jsx
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import "./Checkdone-modal.css";
 import { MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md"
 
-const Modal = () => {
+const Checkdone_modal = (data) => {
   const [modal, setModal] = useState(false);
+  const [Organization, setOrganization] = useState();
+  const [Technology, setTechnology] = useState();
+  const [Idea, setIdea] = useState();
+  const [Communication, setCommunication] = useState();
+  const [Product, setProduct] = useState();
   const toggleModal = () => {
     setModal(!modal);
   };
@@ -18,7 +23,15 @@ const Modal = () => {
   } else {
     document.body.classList.remove('active-modal')
   }
-
+  const handleAdd = () => {
+    axios.post('http://localhost:5000/upscore', {content:content, rank:rank, Project_id: data.project_id, user_id:user_id})
+    .then(result=> {
+      if(result){
+        location.reload()
+      }
+    })
+    .catch(err => console.log(err))
+}
   return (
     <>
       <button onClick={toggleModal} className="btn-modalcheck">
@@ -33,34 +46,34 @@ const Modal = () => {
                 <h2>Task</h2>
             </div>
             <div className="content-task-project">
-                <h3>Content of the task: Do CSS</h3>
-                <h3>Rank of the task: C</h3>
+                <h3>Content of the task: {data.content}</h3>
+                <h3>Rank of the task: {data.rank}</h3>
                 <h3>Score: 10</h3>
             </div>
-            <form action="">
+            <form onSubmit={handleAdd}>
                 <div className="content-plus-points">
                     <h3>Which skills do you want to add?</h3>
                 </div>
                 <div className="plusploint-form">
                     <div className="plus">
                         <label htmlFor="">Organization skill:</label>
-                        <input type="text" required />
+                        <input type="text"onChange={ (e) => setOrganization(e.target.value)} required />
                     </div>
                     <div className="plus">
                         <label htmlFor="">Technology skill:</label>
-                        <input type="text" required />
+                        <input type="text" onChange={ (e) => setTechnology(e.target.value)} required />
                     </div>
                     <div className="plus">
                         <label htmlFor="">Idea contribution:</label>
-                        <input type="text" required />
+                        <input type="text" onChange={ (e) => setIdea(e.target.value)} required />
                     </div>
                     <div className="plus">
                         <label htmlFor="">Communication skill:</label>
-                        <input type="text" required />
+                        <input type="text" onChange={ (e) => setCommunication(e.target.value)} required />
                     </div>
                     <div className="plus">
                         <label htmlFor="">Product optimization:</label>
-                        <input type="text" required />
+                        <input type="text" onChange={ (e) => setProduct(e.target.value)} required />
                     </div>
                 </div>
             </form>
@@ -80,4 +93,4 @@ const Modal = () => {
   );
 }
 
-export default Modal
+export default Checkdone_modal
