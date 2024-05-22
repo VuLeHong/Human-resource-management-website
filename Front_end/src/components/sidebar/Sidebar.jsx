d54f60a54df18b509d0ed4554324b31fb3424379
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-05-22 13:34:39 +0700

    final fix

--- Front_end/src/components/sidebar/Sidebar.jsx
+++ Front_end/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'  
 import { BsFillPersonFill, BsListTask, BsClipboard2Pulse, BsFillArchiveFill } from 'react-icons/bs'
-import logo from '../../assets/logo-white.png'
+import logo from "../../assets/images.png";
 import { Link, useNavigate } from 'react-router-dom'
 import './Sidebar.css'
 
@@ -26,7 +26,7 @@ function Sidebar({openSidebarToggle}) {
     <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
         <div className='sidebar-title'>
             <div className='sidebar-brand'>
-                <img src={logo} height = "50" width = "180" />
+                <img src={logo} />
             </div>
         </div>
 
