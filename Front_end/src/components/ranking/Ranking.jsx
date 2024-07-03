27672968c9a8eb6be4c23bbed2c05e3566d0a271
VuLeHong <Catcanroll104@gmail.com>
2024-07-03 15:30:24 +0700

    .

--- Front_end/src/components/ranking/Ranking.jsx
+++ Front_end/src/components/ranking/Ranking.jsx
@@ -28,7 +28,7 @@ const Ranking = () => {
 
   const [owner, setOwner] = useState({});
   useEffect(() => {
-   axios.post('http://localhost:5000/get', {user_id: auth1.user_id}) 
+   axios.post(`${URL}/get`, {user_id: auth1.user_id}) 
    .then(result => {
            setOwner(result.data)
            console.log(owner.stats)
